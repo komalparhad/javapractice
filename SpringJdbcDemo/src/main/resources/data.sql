a95169e905640af9948a4cdf14f91e685838d1f2
komalparhad <kpankar@ptc.com>
2024-03-08 19:04:35 +0530

    File deleted java

--- SpringJdbcDemo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into alien(id,name,tech) values(5,'kiran','Java');
-insert into alien(id,name,tech) values(6,'pooja','C');
-insert into alien(id,name,tech) values(7,'GK','Java');
--- SpringJdbcDemo/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-create table alien(
-id int PRIMARY KEY,
-name varchar(33),
-tech varchar(33)
-);
-
-insert into alien(id,name,tech) values(1,'kiran','Java');
-insert into alien(id,name,tech) values(2,'pooja','C');
-insert into alien(id,name,tech) values(3,'GK','Java');
